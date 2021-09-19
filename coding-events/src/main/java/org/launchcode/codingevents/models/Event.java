236a0d27eda75a200abef5f61a04dd8c7d64bfac
krystalwhite <krystalsnowhite@gmail.com>
2021-09-19 13:30:38 -0500

    did 1st validation video

--- coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,4 +1,7 @@
 package org.launchcode.codingevents.models;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import java.util.Objects;
 
@@ -7,12 +10,20 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @Size(min=3, max=50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @Email(message = "Invalid email, try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -35,6 +46,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
