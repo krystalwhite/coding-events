c5559aa665e572bc28f079796538aba02c92487f
krystalwhite <krystalsnowhite@gmail.com>
2021-09-21 18:33:14 -0500

    enum video

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,6 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -27,6 +28,7 @@ import java.util.List;
         public String displayCreateEventForm(Model model) {
             model.addAttribute("title", "Create Event");
             model.addAttribute(new Event()); //passing empty event object because it has information about the parameters
+            model.addAttribute("types", EventType.values()); //returns array of 4 constant values in the EventType
             return "events/create";
         }
 
