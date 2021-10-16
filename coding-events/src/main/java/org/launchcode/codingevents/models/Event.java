6de358b683842f0bc93e2a604ad4169e8b7ad9d7
krystalwhite <krystalsnowhite@gmail.com>
2021-10-16 10:25:24 -0500

    creating many to many relationship between event and tag

--- coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,13 +1,12 @@
 package org.launchcode.codingevents.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Event extends AbstractEntity{
@@ -24,6 +23,9 @@ public class Event extends AbstractEntity{
     @ManyToOne
     @NotNull(message = "Category is required.")
     private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -54,6 +56,14 @@ public class Event extends AbstractEntity{
 
     public void setEventDetails(EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
