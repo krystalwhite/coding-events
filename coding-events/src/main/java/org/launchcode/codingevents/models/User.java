0126602eac51e2e40ce4436e4ec202357169c02d
krystalwhite <krystalsnowhite@gmail.com>
2021-10-29 13:07:26 -0500

    starting process for authentication

---
+++ coding-events/src/main/java/org/launchcode/codingevents/models/User.java
@@ -0,0 +1,28 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+//    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+}
+
